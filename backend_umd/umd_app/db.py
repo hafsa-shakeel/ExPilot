fb32cd9219eae10feec4d2a314fca27f92fbf43d
Hafsa <156320505+hafsa-shakeel@users.noreply.github.com>
2025-08-09 03:20:07 +0500

    Create db.py

---
+++ backend_umd/umd_app/db.py
@@ -0,0 +1,22 @@
+import os
+import pyodbc
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_connection():
+    try:
+        conn = pyodbc.connect(
+            f"DRIVER={os.getenv('DB_DRIVER')};"
+            f"SERVER={os.getenv('DB_SERVER')};"
+            f"DATABASE={os.getenv('DB_DATABASE')};"
+            "Trusted_Connection=yes;"
+        )
+        print("Database connected")
+        return conn
+    except Exception as e:
+        print("Database connection failed:", e)
+        return None
+
+if __name__ == "__main__":
+    get_connection()
