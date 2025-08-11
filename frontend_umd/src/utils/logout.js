146bdd16c8885259a39860ddcb1577124e2d7d93
Hafsa <156320505+hafsa-shakeel@users.noreply.github.com>
2025-08-11 20:51:47 +0500

    Add files via upload

--- frontend_umd/src/utils/logout.js
+++ frontend_umd/src/utils/logout.js
@@ -1,1 +1,11 @@
 
+export const logout = () => {
+    localStorage.clear(); // Remove token from storage
+    window.location.href = '/login'; // Redirect to login or home page
+
+    // Optionally clear other session data if stored
+    //localStorage.removeItem('user');
+
+    // Redirect to login page
+    //window.location.href = '/login';
+};
